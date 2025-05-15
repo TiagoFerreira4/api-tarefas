351028a2a26033dc7cb7902468e19531f35e64bb
Tiago Albuquerque Ferreira <tiago.ferreira0410@gmail.com>
2025-05-15 17:23:26 -0300

    Criacao do JwtUtil e do LoginRequest

---
+++ src/main/java/com/tiago/tarefas/security/JwtUtil.java
@@ -0,0 +1,42 @@
+package com.tiago.tarefas.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "chave-secreta-super-segura";
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 horas
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Claims claims = getClaims(token);
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
